4dbd6456bac8dde49ce9fdb175ef43364b782e45
Ahmed Fathy <aliraqi.dev@gmail.com>
2019-03-26 16:58:33 +0200

    add includable scope

---
+++ src/Models/Scopes/Includable.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Elnooronline\LaravelConcerns\Models\Scopes;
+
+use Illuminate\Database\Eloquent\Builder;
+
+/**
+ * Trait Includable.
+ * @method static \Illuminate\Database\Eloquent\Builder withQueryRelations()
+ *
+ */
+trait Includable
+{
+    /**
+     * Load the relations from url query parameters.
+     *
+     * @param Builder $builder
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeWithQueryRelations(Builder $builder)
+    {
+        $includes = $this->getIncludes();
+
+        return $builder->with($includes);
+    }
+
+    /**
+     * Load the relations from url query parameters.
+     *
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function loadQueryRelations()
+    {
+        return $this->load($this->getIncludes());
+    }
+
+    /**
+     * Get the relations from url.
+     *
+     * @return array
+     */
+    private function getIncludes()
+    {
+        return array_filter(explode(',', request()->input('includes')));
+    }
+}

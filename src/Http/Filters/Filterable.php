6606c6c1fbb72b37aa83754ed5b1822e710d1964
Ahmed Fathy <aliraqi.dev@gmail.com>
2019-05-05 15:06:44 +0200

    formating

--- src/Http/Filters/Filterable.php
+++ src/Http/Filters/Filterable.php
@@ -2,19 +2,24 @@
 
 namespace Elnooronline\LaravelConcerns\Http\Filters;
 
+use Illuminate\Support\Facades\App;
+
+/**
+ * @method static \Illuminate\Database\Eloquent\Builder filter(BaseFilters $filters = null)
+ */
 trait Filterable
 {
     /**
      * Apply all relevant thread filters.
      *
      * @param  \Illuminate\Database\Eloquent\Builder $query
-     * @param  BaseFilters $filters
+     * @param  \Elnooronline\LaravelConcerns\Http\Filters\BaseFilters $filters
      * @return \Illuminate\Database\Eloquent\Builder
      */
     public function scopeFilter($query, BaseFilters $filters = null)
     {
         if (! $filters) {
-            $filters = app()->make($this->filter);
+            $filters = App::make($this->filter);
         }
 
         return $filters->apply($query);
